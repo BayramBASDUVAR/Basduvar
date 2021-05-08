95eda6b35ac13f972dd2179b270453b529d6ba6d
DELL <DELL@DESKTOP-HLH6HUP>
2021-05-09 00:25:17 +0300

    Asp.Net Core API/WEB

---
+++ Basduvar.Core/Repositories/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basduvar.Core.Repositories
+{
+    interface IRepository<TEntity> where TEntity : class
+    {
+        Task AddAsync(TEntity entity);
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
+        Task<TEntity> GetByIdAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate);
+        Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
+        TEntity Update(TEntity entity);
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entities);
+
+
+    }
+}
