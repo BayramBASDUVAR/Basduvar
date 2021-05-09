bc48ddf24edaf977fb86beb075da9e26534b58a9
DELL <DELL@DESKTOP-HLH6HUP>
2021-05-10 02:40:53 +0300

    Core: IUnitOfWork interface i  eklendi

---
+++ Basduvar.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using Basduvar.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basduvar.Core.UnitOfWorks
+{
+    interface IUnitOfWork
+    {
+        IProductRepository Products { get; }
+        ICategoryRepository Categories { get; }
+        Task CommitAsync();
+        void Commit();
+    }
+}
