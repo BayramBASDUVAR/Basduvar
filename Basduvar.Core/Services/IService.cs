5c7420626c823a9a649a8d4dd77a09f306d1fb27
DELL <DELL@DESKTOP-HLH6HUP>
2021-06-23 00:43:25 +0300

    Service: ProductService

--- Basduvar.Core/Services/IService.cs
+++ Basduvar.Core/Services/IService.cs
@@ -8,11 +8,11 @@ namespace Basduvar.Core.Services
 {
     public interface IService<TEntity> where TEntity:class
     {
-        Task AddAsync(TEntity entity);
-        Task AddRangeAsync(IEnumerable<TEntity> entities);
+        Task<TEntity> AddAsync(TEntity entity);
+        Task<IEnumerable<TEntity>> AddRangeAsync(IEnumerable<TEntity> entities);
         Task<TEntity> GetByIdAsync(int id);
         Task<IEnumerable<TEntity>> GetAllAsync();
-        Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate);
+        Task<IEnumerable<TEntity>> Where(Expression<Func<TEntity, bool>> predicate);
         Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
         TEntity Update(TEntity entity);
         void Remove(TEntity entity);
