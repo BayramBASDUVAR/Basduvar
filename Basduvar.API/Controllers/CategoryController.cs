9f92f679214583581fb6c4065968c4d1cbf30cd7
DELL <DELL@DESKTOP-HLH6HUP>
2021-07-19 04:12:26 +0300

    API: Category controller method ları yazıldı

--- Basduvar.API/Controllers/CategoryController.cs
+++ Basduvar.API/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Basduvar.API.DTOs;
+using Basduvar.Core.Models;
 using Basduvar.Core.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -27,6 +28,40 @@ namespace Basduvar.API.Controllers
         {
             var categories = await _categoryService.GetAllAsync();
             return Ok(_mapper.Map<IEnumerable<CategoryDto>>(categories));
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var category = await _categoryService.GetByIdAsync(id);
+            return Ok(_mapper.Map<CategoryDto>(category));
+        }
+
+        [HttpGet("{id}/products")]
+        public async Task<IActionResult> GetWithProductsById(int id)
+        {
+            var category = await _categoryService.GetWithProductsByIdAsync(id);
+            return Ok(_mapper.Map<CategoryWithProductDto>(category));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Save(CategoryDto categoryDto)
+        {
+            var newCategory = await _categoryService.AddAsync(_mapper.Map<Category>(categoryDto));
+            return Created(String.Empty,_mapper.Map<CategoryDto>(newCategory));
+        }
+
+        [HttpPut]
+        public IActionResult Update(CategoryDto categoryDto)
+        {
+            var category = _categoryService.Update(_mapper.Map<Category>(categoryDto));
+            return NoContent();//Gereksiz veri trafiği olmasın. Best practice açısından bu şekilde olması gerekir
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Remove(int id)
+        {
+            var category = _categoryService.GetByIdAsync(id).Result;//Result ile asenkron metod senkron oldu
+            _categoryService.Remove(category);
+            return NoContent();
         }
     }
 }
